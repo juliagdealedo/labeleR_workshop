f0e3f6f0788f708342d4a5c73bdf59c4fb02fdad
Julia G. de Aledo <75074514+juliagdealedo@users.noreply.github.com>
2024-01-23 17:38:34 +0100

    Add files via upload
    files and code needed for workshop

---
+++ labeleR_gettingstarted.R
@@ -0,0 +1,35 @@
+
+#RUN EVERYTHING
+{
+path <- tempdir()
+setwd(path)
+
+# Loading necessary packages ####
+if(!require(devtools)){ 
+  install.packages("devtools")
+  #only needed if it is not in CRAN
+}
+
+if(!require(tinytex)){
+tinytex::install_tinytex()
+  #install tinytex if it is not installed
+}
+
+#installing labeleR ####
+devtools::install_github("EcologyR/labeleR")
+library(labeleR)
+
+
+
+
+create_collection_label(
+  data = collection.table,
+  path = "labeleR_output",
+  filename = "labels",
+  field1.column = "field1",
+  bgcolor = "FFFFFF",  #White is "FFFFFF",
+  textcolor = "000000" #Black is "000000"
+)
+
+setwd("~")
+}
